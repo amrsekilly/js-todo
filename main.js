e68af17619b091333f1d85c45a725f6a4fd772a7
Amr Sekilly <amr@pelcro.com>
2020-06-21 22:57:43 +0200

    Refactored the delete functionality

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ const inputField = document.querySelector("#task-name");
 
 let todos = [];
 
+const removeTodo = (listItem) => {
+  const newTodos = todos.filter((todo) => todo !== listItem.innerText);
+  todos = newTodos;
+  renderTodos();
+};
+
 const renderTodos = () => {
   container.innerHTML = "";
   todos.forEach((todo) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = todo;
     container.appendChild(listItem);
-    listItem.addEventListener("click", (e) => {
-      const newTodos = todos.filter((todo) => todo !== listItem.innerText);
-      todos = newTodos;
-      renderTodos();
-    });
+    listItem.addEventListener("click", (e) => removeTodo(listItem));
   });
 };
 
